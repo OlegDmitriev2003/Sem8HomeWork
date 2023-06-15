b1cb3ee095ff315d425d952213fe3b8eb47bb61c
OlegDmitriev <odimitrovgrad@gmail.com>
2023-06-15 17:38:49 +0300

    Completed Task62

---
+++ Task62/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+int num = 1;
+int n = 0;
+int i = 0;
+
+
+Console.WriteLine("Введите размер матрицы: ");
+int size = int.Parse(Console.ReadLine()!);
+int [,] matrix = new int [size, size];
+
+while (num <= size*size)
+{
+    matrix [i,n] = num;
+    if (i <= n+1 && i+n < size-1)
+        n++;
+    else if (i < n && i + n>= size-1)
+        i++;
+    else if (i >= n && i + n > size -1)
+        n--;
+    else
+        i--;
+    num++;
+}
+
+for (int z = 0; z < size; z++)
+    {
+        for (int v = 0; v < size; v++)
+            {
+                Console.Write($"{matrix[z,v]} ");
+            }
+        Console.WriteLine();
+    }
