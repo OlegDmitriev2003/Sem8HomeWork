e1de892af38cb74113845febe64f255006585db5
OlegDmitriev <odimitrovgrad@gmail.com>
2023-06-15 17:11:59 +0300

    completed Task60

---
+++ Task60/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Введите размер трёхмерной матрицы: ");
+int size = int.Parse (Console.ReadLine()!);
+int [,,] matrix = new int [size, size, size];
+
+for(int i = 0; i < size; i++)
+{
+    for(int n = 0; n < size; n++)
+        {
+            for(int h = 0; h< size; h++)
+                {
+                    matrix [i,n,h] = new Random().Next(0, 100);
+                    Console.Write($"{matrix[i,n,h]} ({i},{n},{h}) ");
+                }
+   
+     }
+Console.WriteLine();
+}
